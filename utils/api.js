084da42b0975f1253aefce33707c9d328b52f895
dilip27m <mdilip2909@gmail.com>
2026-01-20 18:21:42 +0530

    implemented env file

--- utils/api.js
+++ utils/api.js
@@ -1,22 +1,34 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', 
+  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000/api',
 });
 
-// Add a request interceptor
+
 api.interceptors.request.use(
   (config) => {
-    // Check if token exists in localStorage
     const token = localStorage.getItem('token');
-    
-    // If token exists, add it to the headers
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
   },
   (error) => {
+    return Promise.reject(error);
+  }
+);
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+        localStorage.removeItem('token');
+        localStorage.removeItem('adminClassId');
+        
+        if (window.location.pathname.startsWith('/admin')) {
+            window.location.href = '/admin/login';
+        }
+    }
     return Promise.reject(error);
   }
 );
